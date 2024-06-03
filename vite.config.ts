b78a9f95d25d02be1d28e69c55ce27bf9e2dab56
aman <aman@oaza.energy>
2024-06-03 13:44:20 +0530

    added yml and update vite config to create chatEmbed.js as a build file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
+    assetsDir: 'assets', // Specify the directory for static assets
+    rollupOptions: {
+      // You can further customize the generated build here if needed
+      output: {
+        // Specify the name of the generated bundle
+        entryFileNames: 'chatEmbed.js', // You can use [hash] or other placeholders for unique names
+        chunkFileNames: 'chatEmbedChunk.js',
+      },
+    },
+  },
 })
