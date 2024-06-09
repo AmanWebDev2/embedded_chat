6160fcd4d940627520767fbc33dc665e9d66a98a
aman <aman@oaza.energy>
2024-06-09 18:30:49 +0530

    added optional chaining

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ export const getIframeNode = (id: string): null | HTMLDivElement => {
     const chatWidgetIframe = document.getElementById(
       "chat-widget-iframe"
     ) as HTMLIFrameElement;
-    const doc = chatWidgetIframe.contentWindow?.document;
+    const doc = chatWidgetIframe?.contentWindow?.document;
     if (!doc) return null;
     const div = doc.getElementById(id);
     return div as HTMLDivElement;
